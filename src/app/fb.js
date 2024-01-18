409f15f3f029802cd86483487321ba48083c5641
roy2pushkar <roy2000pushkar@gmail.com>
2024-01-18 12:15:37 +0530

    Added some features

--- src/app/fb.js
+++ src/app/fb.js
@@ -22,5 +22,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const auth = getAuth(app);
-
+if (typeof window !== "undefined") {
+  const analytics = getAnalytics(app);
+}
 export { app, analytics, auth };

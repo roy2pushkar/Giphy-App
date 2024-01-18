c94beb4faff92799967e0fa65e2044a9d78ae685
roy2pushkar <roy2000pushkar@gmail.com>
2024-01-18 11:52:21 +0530

    Added some features

--- src/app/Navigation.tsx
+++ src/app/Navigation.tsx
@@ -1,39 +1,36 @@
-import Link from "next/link";
 import React from "react";
+import Link from "next/link";
+
 interface NavLink {
-    name:string;
-    path:string;
-    onClick?:void;
+  name: string;
+  path: string;
+  onClick?: () => void; // Assuming onClick is a function
 }
+
 interface NavigationProps {
-    links: NavLink[];
-
+  links: NavLink[];
 }
 
+const Navigation: React.FC<NavigationProps> = ({ links }) => {
+  return (
+    <nav>
+      <ul>
+        {links.map((link, index) => (
+          <li key={index}>
+            {link.onClick ? (
+              <a className="text-sm text-white cursor-pointer" onClick={link.onClick}>
+                {link.name}
+              </a>
+            ) : (
+              <Link href={link.path}>
+                <a className="text-white text-sm">{link.name}</a>
+              </Link>
+            )}
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
-const Navigation = ({links :any}) => {
-    return (
-        <nav>
-            <ul>
-                {links.map((link,index) => (
-                    <li key={index}>
-                        
-                    {link.onclick ? (
-                        <a className="text-sm text-white cursor-pointer">
-                        {link.name}
-                        </a>
-
-                    ):(
-                        <link href={link.path}>
-                             <a className="text-white text-sm">{link.name}</a>
-                        </link>
-                    )}
-
-                    </li>
-                ))}
-            </ul>
-        </nav>
-    )
-   
-}
- export default Navigation ;
+export default Navigation;
